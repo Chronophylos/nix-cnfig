2c1b0233933b79f8f925f2c9e208838454a93dba
Nikolai Zimmermann <nikolai@chronophylos.com>
2024-05-20 16:59:56 +0200

    update

---
+++ overlays/chatterino7.nix
@@ -0,0 +1,12 @@
+final:
+prev
+{
+  chatterino2 = prev.chatterino2.overrideAttrs (old: {
+    src = prev.fetchFromGithub {
+      owner = "SevenTV";
+      repo = "chatterino7";
+      rev = "a3ef8a138037924f9928f2d67cd96b1801062676";
+      hash = "";
+    };
+  });
+}
--- overlays/default.nix
+++ overlays/default.nix
@@ -12,6 +12,8 @@
     # });
   };
 
+  chatterino2-7tv = final: prev: import ./chatterino7.nix;
+
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.unstable'
   unstable-packages = final: _prev: {
