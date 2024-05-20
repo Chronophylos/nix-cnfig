61490ba4790228237b6912b7b8fbdd256c9407d8
Nikolai Zimmermann <nikolai@chronophylos.com>
2024-05-20 19:29:26 +0200

    update

--- overlays/chatterino7.nix
+++ overlays/chatterino7.nix
@@ -8,5 +8,6 @@ final: prev: {
       hash = "sha256-R/y1dZo/wevBiatBD3GZ52QAZ4NK5uG7gGKUjCHBlMA=";
       fetchSubmodules = true;
     };
+    nativeBuildInputs = old.nativeBuildInputs or [] ++ [prev.git];
   });
 }
