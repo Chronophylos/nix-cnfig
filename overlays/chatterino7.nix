2c9b2f3f322ceb64b39d1b9495460fa47c5355de
Nikolai Zimmermann <nikolai@chronophylos.com>
2024-05-20 17:03:38 +0200

    update

--- overlays/chatterino7.nix
+++ overlays/chatterino7.nix
@@ -1,6 +1,6 @@
 final: prev: {
   chatterino2 = prev.chatterino2.overrideAttrs (old: {
-    src = prev.fetchFromGithub {
+    src = prev.fetchFromGitHub {
       owner = "SevenTV";
       repo = "chatterino7";
       rev = "a3ef8a138037924f9928f2d67cd96b1801062676";
